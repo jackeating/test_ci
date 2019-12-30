78e18ea155ceaa523d1c3787b415b237684cf5dc
github jackeating <nomovienolife0209@gmail.com>
2019-12-30 22:37:14 +0900

    forth commit

--- test_calculations.py
+++ test_calculations.py
@@ -7,7 +7,7 @@ class CaluclationsTest(unittest.TestCase):
         self.assertEqual(calculations.divide(2,2), 1)
         
     def test_divide_contain_zero(self):
-        self.assertEqual(calculations.divide(0, 1), 1)
+        self.assertEqual(calculations.divide(0, 1), 0)
         
 if __name__ == "__main__":
     unittest.main()
